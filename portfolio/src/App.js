0488f04ee51b347c7756a5b766507285dc757d93
Michael Rockingham <mrockingham2@gmail.com>
2020-10-26 17:15:58 -0400

    Set up react-app, pages folder, and started gtid

---
+++ portfolio/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Main from './pages/Main';
+
+function App() {
+  return (
+    <div className="app">
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/pages/Main.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Main = () => {
+    return (
+        <div>
+            <div className = 'grid-container'>
+                <main className= 'user-info'>
+                    Hi, I'm Mike
+                </main>
+                <section className='sec-1'>
+                    sec 1
+                </section >
+                <section className='sec-2'>
+                    sec-1
+                </section>
+                <header>
+                    header
+                </header>
+                <div className='sec-3'>
+                    sec-3
+                </div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Main
