5c77f4af3b176371ca709a336ef0469b8dc85604
Michael Rockingham <mrockingham2@gmail.com>
2020-10-26 23:26:29 -0400

    Added Main title info, portfolio pic, some animations

--- portfolio/src/pages/Main.js
+++ portfolio/src/pages/Main.js
@@ -1,23 +1,28 @@
 import React from 'react'
+import './main.css'
 
 const Main = () => {
     return (
         <div>
             <div className = 'grid-container'>
                 <main className= 'user-info'>
-                    Hi, I'm Mike
+                    <h1>Hi,</h1>
+                    
                 </main>
                 <section className='sec-1'>
-                    sec 1
+                    <h2>My name is Michael Rockingham. I'm a full-Stack Developoer based out of Orlando, looking to build great projects. </h2>
                 </section >
                 <section className='sec-2'>
-                    sec-1
+                    <h2>Contact Me</h2>
                 </section>
                 <header>
-                    header
+                    <div>Home</div>
+                    <div>Projects</div>
+                    <div>Contact Me</div>
+                    
                 </header>
                 <div className='sec-3'>
-                    sec-3
+                    
                 </div>
                 
             </div>
