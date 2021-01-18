7af43f8763e19920be62c194c1b2a353d6f2feb8
Michael Rockingham <mrockingham2@gmail.com>
2021-01-18 11:37:59 -0500

    added amazon app to porfolio

--- portfolio/src/compontent/MyProject.js
+++ portfolio/src/compontent/MyProject.js
@@ -6,6 +6,7 @@ import Slack from '../img/slack-app.PNG'
 import conway from '../img/conway.PNG'
 import dad from '../img/dad-app.png'
 import dataTable from '../img/data.PNG'
+import amazonClone from '../img/amazonclone.PNG'
 
 
 
@@ -28,7 +29,7 @@ const MyProject = () => {
             <ReactBootStrap.Card.Img variant="top" src={Slack} />
             <ReactBootStrap.Card.Body style={{ color: "white", backgroundColor:'black', border:' 1px solid' }}>
               <ReactBootStrap.Card.Title>Slack App</ReactBootStrap.Card.Title>
-              <ReactBootStrap.Card.Text>
+              <ReactBootStrap.Card.Text style={{paddingBottom: '10%'}}>
                 Slack like app, built using React, Firestore, Google Authentication, SQL
               </ReactBootStrap.Card.Text>
               <div className='button-group'>
@@ -46,13 +47,13 @@ const MyProject = () => {
         </div>
         <div className='p2'>
         <ReactBootStrap.Card style={{ width: "20rem", border:'none', backgroundColor: 'black'  }}>
-          <ReactBootStrap.Nav.Link href='https://fam-app1.vercel.app/'>
-            <ReactBootStrap.Card.Img variant="top" src={expense} />
-            <ReactBootStrap.Card.Body style={{ color: "white", backgroundColor:'black', border:'1px solid', height:'200px'  }}>
-              <ReactBootStrap.Card.Title>Expence App</ReactBootStrap.Card.Title>
-              <ReactBootStrap.Card.Text style={{paddingBottom: '10%'}}>
-                Expence app, built useing React. Backend built using MongoDB.
-              </ReactBootStrap.Card.Text>
+          <ReactBootStrap.Nav.Link  href='https://clone-72250.firebaseapp.com/'>
+            <ReactBootStrap.Card.Img variant="top" src={amazonClone}  />
+            <ReactBootStrap.Card.Body style={{ color: "white", backgroundColor:'black', border:'1px solid'  }}>
+              <ReactBootStrap.Card.Title>Amazon Clone</ReactBootStrap.Card.Title>
+              <ReactBootStrap.Card.Text  >
+                Amazon like app, built with React, Context-Api, Material-ui, Google Firebase, and Google Authentication .
+              </ReactBootStrap.Card.Text >
               <div className='button-group'>
               <ReactBootStrap.Button variant="primary">
                 App
@@ -73,11 +74,11 @@ const MyProject = () => {
         <ReactBootStrap.Card style={{ width: "20rem", border:'none', height:'100%', backgroundColor: 'black', fontWeight: '900'  }}>
         <ReactBootStrap.Nav.Link href='https://conway-s-mike-s-game.vercel.app/'>
             <ReactBootStrap.Card.Img style={{height:'180px'}} variant="top" src={conway} />
-            <ReactBootStrap.Card.Body style={{ color: "white", backgroundColor:'black', border:'1px solid', height:'200px'  }}>
+            <ReactBootStrap.Card.Body style={{ color: "white", backgroundColor:'black', border:'1px solid'  }}>
               <ReactBootStrap.Card.Title >Conway Game of Life</ReactBootStrap.Card.Title>
-              <ReactBootStrap.Card.Text>
+              <ReactBootStrap.Card.Text style={{paddingBottom: '10%'}}>
                 Conways game of life, built with React, Framer-motion, Material Ui
-              </ReactBootStrap.Card.Text>
+              </ReactBootStrap.Card.Text >
               <div className='button-group'>
               <ReactBootStrap.Button variant="primary">
                 App
@@ -97,7 +98,7 @@ const MyProject = () => {
             <ReactBootStrap.Card.Img variant="top" src={dad} />
             <ReactBootStrap.Card.Body style={{ color: "white", backgroundColor:'black', border:'1px solid'  }}>
               <ReactBootStrap.Card.Title>Dad's Memory</ReactBootStrap.Card.Title>
-              <ReactBootStrap.Card.Text >
+              <ReactBootStrap.Card.Text style={{paddingBottom: '10%'}}>
                 Memorial app for my DAd, built with React, Firebase, BootStrap, and Framoer-motion
               </ReactBootStrap.Card.Text>
               <div className='button-group'>
@@ -120,7 +121,7 @@ const MyProject = () => {
             <ReactBootStrap.Card.Img variant="top" src={dataTable} style={{height: '180px'}} />
             <ReactBootStrap.Card.Body style={{ color: "white", backgroundColor:'black', border:'1px solid'  }}>
               <ReactBootStrap.Card.Title>Bridges To Prosperity</ReactBootStrap.Card.Title>
-              <ReactBootStrap.Card.Text >
+              <ReactBootStrap.Card.Text style={{paddingBottom: '10%'}} >
                 Group Project for a non- profit, built with Context Api, Okta, React-Map-Gl, Material UI
               </ReactBootStrap.Card.Text>
               <div className='button-group'>
